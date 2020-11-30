305a02ce9452ad42e0d14dc87f93e5cd25481de7
shiwenqiang <qq03768123883>
2020-11-30 14:49:00 +0800

    修改数据库和表配置

--- src/main/java/com/example/entity/User.java
+++ src/main/java/com/example/entity/User.java
@@ -5,6 +5,6 @@ import lombok.Data;
 @Data
 public class User {
     private String userId;
-    private String userName;
-    private String password;
+    private String username;
+    private String userPassword;
 }
